a53b709863340d53f104b68a0dbd9f3e1b47d18c
Artur Pioro <artupior@gmail.com>
2016-01-05 00:03:45 +0100

    - Changed comments. - Restructured destination specification and exclude-from - Lock file removed through trap

--- brsync2nas.sh
+++ brsync2nas.sh
@@ -1,47 +1,74 @@
 #!/bin/bash
 #
-#  brsync2nas -- Simple shell script for NAS Backup
+#  brsync2nas.sh - Simple shell script for Backup with RSYNS to NAS
 #
-# SRCD - Source directory to Backup
-# DSTNASMP - Desstination NAS mount point
-# DSTD - Destination directory - assumes it is available
+#  SYNOPSIS
+#    brsync2nas.sh [<SRCD>  [<DSTNASMP> [<DSTNASMPSUBD>]]]
+#
+# SRCD - Top of directory tree to be backed up
+# DSTNASMP - NFS Mount point of destination NAS (needs to be mounted)
+# DSTNASMPSUBD - Subidrectory within NAS to be used (by default null - ".")
+#
+#
 # LOGF - Log file location
-# EXCLUDE - File with exclusions
+# EXCLUDE - File with exclusions (rsync pattern format)
 
-SRCD=/home
-BR2ND=$SRCD/.$(basename $0)
-LOGF=$BR2ND/log
-LOCKD=$BR2ND/lock
-EXCLUDE=$BR2ND/exclude
-
-DSTNASMP=/qnap/Backup-$(hostname)
-DSTD=$DSTNASMP
-
-if [ ! -d $SRCD ]; then
-  echo Source directory $SRCD does not exist. Exiting.
+# Top directory to be backed up. No trailing "/"
+SRCD=${1:-"/home"}; SRCD="${SRCD%/}"
+if [[ ! -d $SRCD ]]; then
+  echo Source directory ${SRCD} does not exist. Exiting.
   exit 1
+fi
+
+# Destination NFS mountpoint. No trailing "/"
+DSTNASMP=${2:-"/qnap/Backup-$(hostname)"}; DSTNASMP="${DSTNASMP%/}"
+
+# Subidrectory on NAS.
+DSTNASMPSUBD="$3";
+if [[ -n "${DSTNASMPSUBD}" ]]; then
+  # If set make sure it starts with "/"
+  [[ "${DSTNASMPSUBD}" = "${DSTNASMPSUBD#/}" ]] && DSTNASMPSUBD="/${DSTNASMPSUBD}"
+fi
+
+#Destination dir on NAS - don't change instead set DSTNASMP or DSTNASMPSUBD
+DSTD="${DSTNASMP}${DSTNASMPSUBD}"
+
+#Log, lock and excludes - allow different for different destination
+MYNAME=$(basename $0)
+BR2ND="$SRCD/.${MYNAME%.sh}${DSTNASMP}${DSTNASMPSUBD}"
+LOGF="$BR2ND/log"
+LOCKD="$BR2ND/lock"
+
+# Exclude may be specific to destination or global for source directory tree
+if [[ -f "$BR2ND/exclude" ]]; then
+  EXCLUDEFROM="--exclude-from=$BR2ND/exclude"
+elif [[ -f "$SRCD/.${MYNAME%.sh}/exclude" ]]; then
+  EXCLUDEFROM="--exclude-from=$SRCD/.${MYNAME%.sh}/exclude"
+fi
+
+function finish {
+  rmdir $LOCKD
+}
+
+mkdir -p $BR2ND
+if
+  mkdir $LOCKD 2> /dev/null
+then
+  trap finish EXIT SIGINT SIGTERM SIGKILL
+else
+  echo Cannot create $LOCKD. Insufficient permissions or another ${MYNAME} is still running. Exiting.
+  exit 2
 fi
 
 if
   ! (mount | grep "$DSTNASMP type nfs" > /dev/null)
 then
-  echo NAS to mounted on $DSTNASMP. Exiting.
-  exit 2
-fi
-
-mkdir -p $BR2ND
-if
-  ! mkdir $LOCKD 2> /dev/null
-then
-  echo $LOCKD esists. Potentially another $(basename $0) is still running. Exiting.
+  echo NAS is not mounted on ${DSTNASMP}. Exiting.
   exit 3
 fi
 
-
 exec >> $LOGF 2>&1
-echo $(date +"%D %T") $(basename $0) starting.
-[ -f $EXCLUDE ] && EXCLUDEFROM="--exclude-from=$EXCLUDE"
-rsync -avAHX --delete --exclude=$LOCKD $EXCLUDEFROM $SRCD $DSTD
-rmdir $LOCKD
-echo $(date +"%D %T") $(basename $0) finished.
+echo $(date +"%D %T") ${MYNAME} starting.
+rsync -avH --exclude=$LOCKD $EXCLUDEFROM --delete $SRCD $DSTD
+echo $(date +"%D %T") ${MYNAME} finished.
 exit 0
